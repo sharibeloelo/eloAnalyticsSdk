26fa9be55d1c992bc01bf0ad60a8120624043d8d
ankiteloelo <ankit@eloelo.in>
2025-06-26 17:26:51 +0530

    Analytic SDK

---
+++ app/src/main/java/com/eloelo/analytics/EApplication.kt
@@ -0,0 +1,12 @@
+package com.eloelo.analytics
+
+import android.app.Application
+import com.greenhorn.neuronet.PublishEvent
+
+class EApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
