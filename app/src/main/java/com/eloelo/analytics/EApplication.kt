a979a7c921140795822a968ff37d46588695ddf3
Sharib Ahmed <sharib@eloelo.in>
2025-07-15 20:17:21 +0530

    minor

--- app/src/main/java/com/eloelo/analytics/EApplication.kt
+++ app/src/main/java/com/eloelo/analytics/EApplication.kt
@@ -7,13 +7,9 @@ import android.app.Application
 import androidx.work.Configuration
 import androidx.work.WorkManager
 
-class EApplication : Application(), Configuration.Provider {
+class EApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        WorkManager.initialize(context = this, Configuration.Builder().build())
     }
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder().build()
 }
